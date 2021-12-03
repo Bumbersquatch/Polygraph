359beee6a3f3e6ea963d5f6f20996a7d67a7a91a
Alex Ross <alexross1987@yahoo.co.uk>
2021-12-03 20:44:05 +0000

    Add initial data fetch from gapi

--- src/components/Polygraph.vue
+++ src/components/Polygraph.vue
@@ -1,12 +1,47 @@
 <template>
   <div>
     <h1>Polygraph</h1>
+    <ul>
+      <li v-for="keyword in trendingKeyWords" :key="keyword.word">{{keyword.word}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import  TrendingJs  from  'trendingjs'
+
 export default {
   name: 'Polygraph',
+  data() {
+      return {
+          trendingKeyWords: []
+      }
+  },
+  created() {
+    this.$gapi.getGapiClient().then((res) => {
+        res.client.factchecktools.claims.search({ 
+            query: 'a OR e OR s',
+            languageCode: 'en',
+            maxAgeDays: '7',
+            pageSize: 50
+        }).then((data) => {
+          let headlines = []
+          if (data.result && data.status === 200) {
+            data.result.claims.forEach((item) => {
+              headlines.push(item.text.toLowerCase());
+            })
+
+            this.trendingKeyWords = TrendingJs(headlines, 10, {
+              minWord:  2,
+              wordChain:  1,
+              allowSameSentenceDuplicate:  false,
+              ignoreIrrelevantSingleWords:  true,
+              commonWords: ['shows', 'named', 'released', 'people', 'found', 'suggests', 'picture', 'photo', 'video', 'child']
+            })
+          }
+        })
+    })
+  } 
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueGapi from 'vue-gapi'
 
 Vue.config.productionTip = false
+Vue.use(VueGapi, {
+  apiKey: 'AIzaSyAjICwMw6ppKxv1qUbK4bD-oTcwD5q-tLI',
+  discoveryDocs: ['https://factchecktools.googleapis.com/$discovery/rest?version=v1alpha1']
+})
 
 new Vue({
   render: h => h(App),
